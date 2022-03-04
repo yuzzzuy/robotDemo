478b78d6451b639a3147e8ac3e36b93da65dc529
雨中薰风 <yuzhongxunfeng@gmail.com>
2022-03-04 17:02:54 +0800

    1.update demo

--- src/main/java/com/bot/demo/bot/Main.java
+++ src/main/java/com/bot/demo/bot/Main.java
@@ -1,5 +1,6 @@
 package com.bot.demo.bot;
 
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -11,10 +12,16 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
  */
 public class Main {
     public static void main(String[] args) {
-
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new MyAmazingBot());
+            // Set up Http proxy
+            DefaultBotOptions botOptions = new DefaultBotOptions();
+
+            botOptions.setProxyHost("127.0.0.1");
+            botOptions.setProxyPort(7890);
+            // Select proxy type: [HTTP|SOCKS4|SOCKS5] (default: NO_PROXY)
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+            botsApi.registerBot(new MyAmazingBot("5154611046:AAFIRV8YTUn9wM43L3RaxVO34m4qP55y7kI","nnnnnbbbbbbot",botOptions));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
